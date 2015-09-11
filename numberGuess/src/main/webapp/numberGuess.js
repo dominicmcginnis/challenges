8bc195d21bf388c6719aecc5cfe142985357a859
Dominic McGinnis <dominicmcginnis@gmail.com>
2015-09-11 15:40:56 -0700

    Wiring up API calls

--- numberGuess/src/main/webapp/numberGuess.js
+++ numberGuess/src/main/webapp/numberGuess.js
@@ -1,10 +1,27 @@
 angular.module('numberGuessApp', [])
-  .controller('GuessEntryController', function() {
+  .controller('GuessEntryController', function($scope, $http) {
     var numberGuess = this;
     numberGuess.startGame = function() {
+    	var url = "/api/guess/start";
+   		var responsePromise = $http.get(url);
+   		responsePromise.success(function(data, status, headers, config) {
+   			$scope.serverResponse = data.resp;
+   			var gameId = headers("game-id");
+   			$httpProvider.defaults.headers.get = { 'My-ame-id' : gameId };
+    	});
+    	responsePromise.error(function(data, status, headers, config) {
+    		alert("An error occurred.");
+    	});
 
     };
-    numberGuess.makeGuess = function() {
-    	var guess = yourGuess;
+    numberGuess.makeGuess = function(guess) {
+    	var url = "/api/guess/" + guess;
+   		var responsePromise = $http.get(url);
+   		responsePromise.success(function(data, status, headers, config) {
+   			$scope.serverResponse = data.resp;
+    	});
+    	responsePromise.error(function(data, status, headers, config) {
+    		alert("An error occurred.");    		
+    	});
     };
   });
